384684323a5e43f6dc46eafecff88fb35de69899
Florian Bruggisser <florian@nexpose.ch>
2017-11-19 22:21:13 +0100

    put everything into a timer

--- src/main.cpp
+++ src/main.cpp
@@ -19,12 +19,14 @@
 #include <FastLED.h>
 #include "LEDRing.h"
 #include "MicroUtils.h"
+#include "Timer.h"
 
 #define UPDATES_PER_SECOND 100
 
 #define NUM_LEDS 30
 #define DATA_PIN D6
 
+#define DHT_UPDATE_RATE 300
 #define DHT_PIN D1
 #define DHT_TYPE DHT22
 
@@ -38,9 +40,21 @@ LEDRing leds = LEDRing(NUM_LEDS);
 DHT dht = DHT(DHT_PIN, DHT_TYPE);
 
 CHSV tempColor = fromHSV(0, 100, 100);
+Timer timer = Timer(DHT_UPDATE_RATE);
+
+uint8_t colorIndex = 0;
 
 double mapf(double val, double in_min, double in_max, double out_min, double out_max) {
     return (val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void showRainbow(uint8_t colorIndex) {
+    uint8_t brightness = 255;
+
+    for (int i = 0; i < NUM_LEDS; i++) {
+        leds.leds[i] = ColorFromPalette(RainbowColors_p, colorIndex, brightness, LINEARBLEND);
+        colorIndex += 3;
+    }
 }
 
 void setup() {
@@ -53,34 +67,40 @@ void setup() {
 }
 
 void loop() {
-    delay(250);
+    if (timer.elapsed()) {
+        float h = dht.readHumidity();
+        float t = dht.readTemperature();
 
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
+        if (isnan(h) || isnan(t)) {
+            Serial.println("Failed to read from DHT sensor!");
+            return;
+        }
 
-    if (isnan(h) || isnan(t)) {
-        Serial.println("Failed to read from DHT sensor!");
-        return;
+        float hic = dht.computeHeatIndex(t, h, false);
+        // set color
+        auto hue = static_cast<uint8_t>(min(HUE_MAX, max(HUE_MIN, mapf(hic, RANGE_MIN, RANGE_MAX, HUE_MIN, HUE_MAX))));
+        tempColor.hue = hue;
+
+        // overwrite with rainbow
+        if (hue == 255) {
+            colorIndex = static_cast<uint8_t>((colorIndex + 1) % 255);
+            showRainbow(colorIndex);
+        } else {
+            leds.all(tempColor);
+        }
+
+        Serial.print("Color: ");
+        Serial.print(hue);
+        Serial.print(" Humidity: ");
+        Serial.print(h);
+        Serial.print(" %\t");
+        Serial.print("Temperature: ");
+        Serial.print(t);
+        Serial.print(" *C ");
+        Serial.print("Heat index: ");
+        Serial.print(hic);
+        Serial.print(" *C \n");
     }
-
-    float hic = dht.computeHeatIndex(t, h, false);
-    // set color
-    auto hue = static_cast<uint8_t>(min(HUE_MAX, max(HUE_MIN, mapf(hic, RANGE_MIN, RANGE_MAX, HUE_MIN, HUE_MAX))));
-    tempColor.hue = hue;
-
-    Serial.print("Color: ");
-    Serial.print(hue);
-    Serial.print(" Humidity: ");
-    Serial.print(h);
-    Serial.print(" %\t");
-    Serial.print("Temperature: ");
-    Serial.print(t);
-    Serial.print(" *C ");
-    Serial.print("Heat index: ");
-    Serial.print(hic);
-    Serial.print(" *C \n");
-
-    leds.all(tempColor);
 
     // write leds
     FastLED.show();
